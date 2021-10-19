62ee70a40b93f9e9a473cb27a8eb37154b057d06
anis.amirouche <anis.amirouche@outlook.com>
2021-10-19 18:02:18 +0300

    like feature added

--- api/users/views.py
+++ api/users/views.py
@@ -7,7 +7,8 @@ from .serializers import CrateUserSerializer, UserSerializer
 from rest_framework import parsers
 from django_filters import rest_framework as filters
 from rest_framework import permissions
-
+from django.http import HttpResponse
+import json
 
 
 # Create your views here.
@@ -37,8 +38,9 @@ class UserLikeView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     def post(self, request, pk, format=None):
-        request.user.add(likes=pk)
-        return Response(serializer.data)
+        user_to_like = User.objects.get(id=pk)
+        user_to_like.likes.add(request.user.id)
+        return  HttpResponse(json.dumps({'message': "user liked"}))
 
     
 
